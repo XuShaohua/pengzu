32b3ae6af3ef90498737f33a56b3951aae3d7f01
Xu Shaohua <shaohua@biofan.org>
2022-05-31 08:20:17 +0800

    backend: Add models

---
+++ backend/src/models/mod.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+pub mod authors;
+pub mod books;
+pub mod comments;
+pub mod data;
+pub mod identifiers;
+pub mod languages;
+pub mod publishers;
+pub mod ratings;
+pub mod tags;

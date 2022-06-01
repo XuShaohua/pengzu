1f61b743141d60cfaba5bd12c5fd75caf199ea9d
Xu Shaohua <shaohua@biofan.org>
2022-06-01 10:01:43 +0800

    backend: Impl tags model api

--- backend/src/models/tags.rs
+++ backend/src/models/tags.rs
@@ -2,18 +2,42 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-pub fn add_tag() {
-    todo!();
+use chrono::NaiveDateTime;
+use diesel::{ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl};
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::schema::tags;
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "tags"]
+pub struct NewTag {
+    pub name: String,
 }
 
-pub fn get_tags() {
-    todo!();
+#[derive(Debug, Serialize, Queryable)]
+pub struct Tag {
+    pub id: i32,
+    pub name: String,
+    pub created: NaiveDateTime,
+    pub last_modified: NaiveDateTime,
 }
 
-pub fn get_all_tags() {
-    todo!();
+pub fn add_tag(conn: &PgConnection, new_tag: &NewTag) -> Result<(), Error> {
+    use crate::schema::tags::dsl::tags;
+    diesel::insert_into(tags).values(new_tag).execute(conn)?;
+    Ok(())
 }
 
-pub fn update_tag() {
-    todo!();
+pub fn get_all_tags(conn: &PgConnection) -> Result<Vec<Tag>, Error> {
+    use crate::schema::tags::dsl::tags;
+    tags.load::<Tag>(conn).map_err(Into::into)
+}
+
+pub fn update_tag(conn: &PgConnection, tag_id: i32, new_tag: &NewTag) -> Result<(), Error> {
+    use crate::schema::tags::dsl::{name, tags};
+    diesel::update(tags.find(tag_id))
+        .set(name.eq(new_tag.name.as_str()))
+        .execute(conn)?;
+    Ok(())
 }

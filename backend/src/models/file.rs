cef05f508fc836071eef7ad74e3d53f0e09e82a6
Xu Shaohua <shaohua@biofan.org>
2022-06-01 10:23:55 +0800

    backend: Add file model api

--- backend/src/models/file.rs
+++ backend/src/models/file.rs
@@ -2,18 +2,45 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-pub fn add_file() {
-    todo!();
+use chrono::NaiveDateTime;
+use diesel::{ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl};
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::schema::files;
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "files"]
+pub struct NewFile {
+    pub book: i32,
+    pub format: i32,
+    pub uncompressed_size: i32,
+    pub name: String,
+    pub sha: String,
 }
 
-pub fn get_one_book_files() {
-    todo!();
+#[derive(Debug, Serialize, Queryable)]
+pub struct File {
+    pub id: i32,
+    pub book: i32,
+    pub format: i32,
+    pub uncompressed_size: i32,
+    pub name: String,
+    pub sha: String,
+    pub created: NaiveDateTime,
+    pub last_modified: NaiveDateTime,
 }
 
-pub fn get_books_files() {
-    todo!();
+pub fn add_file(conn: &PgConnection, new_file: &NewFile) -> Result<(), Error> {
+    use crate::schema::files::dsl::files;
+    diesel::insert_into(files).values(new_file).execute(conn)?;
+    Ok(())
 }
 
-pub fn update_file() {
-    todo!();
+pub fn get_book_files(conn: &PgConnection, book_id: i32) -> Result<Vec<File>, Error> {
+    use crate::schema::files::dsl::{book, files};
+    files
+        .filter(book.eq(book_id))
+        .load::<File>(conn)
+        .map_err(Into::into)
 }

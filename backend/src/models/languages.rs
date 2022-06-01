e6d5a7783d9f21b670fd9520818b939ab2b7c91f
Xu Shaohua <shaohua@biofan.org>
2022-06-01 09:40:57 +0800

    backend: Add language model api

--- backend/src/models/languages.rs
+++ backend/src/models/languages.rs
@@ -2,10 +2,33 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-pub fn add_language() {
-    todo!();
+use diesel::{Insertable, PgConnection, RunQueryDsl};
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::schema::languages;
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "languages"]
+pub struct NewLanguage {
+    pub lang_code: String,
 }
 
-pub fn get_all_languages() {
-    todo!();
+#[derive(Debug, Serialize, Queryable)]
+pub struct Language {
+    pub id: i32,
+    pub lang_code: String,
+}
+
+pub fn add_language(conn: &PgConnection, new_language: &NewLanguage) -> Result<(), Error> {
+    use crate::schema::languages::dsl::languages;
+    diesel::insert_into(languages)
+        .values(new_language)
+        .execute(conn)?;
+    Ok(())
+}
+
+pub fn get_all_languages(conn: &PgConnection) -> Result<Vec<Language>, Error> {
+    use crate::schema::languages::dsl::languages;
+    languages.load::<Language>(conn).map_err(Into::into)
 }
--- backend/src/views/languages.rs
+++ backend/src/views/languages.rs
@@ -1,11 +1,3 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
-
-pub async fn add_language() {
-    todo!();
-}
-
-pub async fn get_all_languages() {
-    todo!();
-}

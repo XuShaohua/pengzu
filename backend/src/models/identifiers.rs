7d837e95cfdd4082f93697dae60510880f20a22f
Xu Shaohua <shaohua@biofan.org>
2022-06-01 08:55:26 +0800

    backend: Impl identifier model

--- backend/src/models/identifiers.rs
+++ backend/src/models/identifiers.rs
@@ -2,22 +2,44 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-pub fn add_identifier() {
-    todo!();
+use chrono::NaiveDateTime;
+use diesel::{EqAll, Insertable, PgConnection, QueryDsl, Queryable, RunQueryDsl};
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::schema::identifiers;
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "identifiers"]
+pub struct NewIdentifier {
+    pub book: i32,
+    pub scheme: i32,
+    pub url: Option<String>,
 }
 
-pub fn get_identifiers() {
-    todo!();
+#[derive(Debug, Serialize, Queryable)]
+pub struct Identifier {
+    pub id: i32,
+    pub book: i32,
+    pub scheme: i32,
+    pub value: String,
+    pub url: Option<String>,
+    pub created: NaiveDateTime,
+    pub last_modified: NaiveDateTime,
 }
 
-pub fn get_all_identifiers() {
-    todo!();
+pub fn add_identifier(conn: &PgConnection, new_identifier: &NewIdentifier) -> Result<(), Error> {
+    use crate::schema::identifiers::dsl::identifiers;
+    diesel::insert_into(identifiers)
+        .values(new_identifier)
+        .execute(conn)?;
+    Ok(())
 }
 
-pub fn update_identifier() {
-    todo!();
-}
-
-pub fn delete_identifier() {
-    todo!();
+pub fn get_identifiers(conn: &PgConnection, book_id: i32) -> Result<Vec<Identifier>, Error> {
+    use crate::schema::identifiers::dsl::{book, identifiers};
+    identifiers
+        .filter(book.eq_all(book_id))
+        .load::<Identifier>(conn)
+        .map_err(Into::into)
 }

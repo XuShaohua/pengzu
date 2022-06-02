8a7fbe271a5a9e02053ae34fb1d2141d2fb93313
Xu Shaohua <shaohua@biofan.org>
2022-06-02 14:59:27 +0800

    calibre: Add identifiers table api

---
+++ calibre/src/models/identifiers.rs
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+use diesel::{ExpressionMethods, QueryDsl, Queryable, RunQueryDsl, SqliteConnection};
+
+use crate::error::Error;
+
+#[derive(Debug, Queryable)]
+pub struct Identifier {
+    pub id: i32,
+    pub book: i32,
+    pub type_: String,
+    pub val: String,
+}
+
+pub fn get_identifiers(conn: &SqliteConnection, book_id: i32) -> Result<Vec<Identifier>, Error> {
+    use crate::schema::identifiers::dsl::{book, identifiers};
+    identifiers
+        .filter(book.eq(book_id))
+        .load::<Identifier>(conn)
+        .map_err(Into::into)
+}

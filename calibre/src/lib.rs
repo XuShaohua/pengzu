b1c7cc7b58bd36bc2c5734a8571d780a0cb5a43a
Xu Shaohua <shaohua@biofan.org>
2022-06-02 15:54:25 +0800

    calibre: Add clippy lint options

--- calibre/src/lib.rs
+++ calibre/src/lib.rs
@@ -2,6 +2,16 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic
+)]
+// TODO(Shaohua): Remove this attribute.
+#![allow(dead_code, clippy::module_name_repetitions, clippy::missing_errors_doc)]
+
 #[macro_use]
 extern crate diesel;
 

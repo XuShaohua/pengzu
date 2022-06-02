124256fb04b4b9326813523f8adbab6207da29fc
Xu Shaohua <shaohua@biofan.org>
2022-06-02 15:36:17 +0800

    calibre: Simplify join query in books-ratings link table

--- calibre/src/models/books_ratings.rs
+++ calibre/src/models/books_ratings.rs
@@ -5,28 +5,21 @@
 use diesel::{ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl, SqliteConnection};
 
 use crate::error::Error;
-use crate::models::ratings;
 
 #[derive(Debug, Queryable)]
 pub struct BookRating {
-    pub id: i32,
     pub book: i32,
     pub rating: i32,
 }
 
-#[derive(Debug, Queryable)]
-pub struct BookRatingJoin {
-    pub book: BookRating,
-    pub rating: ratings::Rating,
-}
-
-pub fn get_book_rating(conn: &SqliteConnection, book_id: i32) -> Result<BookRatingJoin, Error> {
+pub fn get_book_rating(conn: &SqliteConnection, book_id: i32) -> Result<BookRating, Error> {
     use crate::schema::books_ratings_link::{self, dsl::book};
     use crate::schema::ratings;
 
     books_ratings_link::dsl::books_ratings_link
         .filter(book.eq(book_id))
         .inner_join(ratings::table.on(books_ratings_link::rating.eq(ratings::id)))
-        .first::<BookRatingJoin>(conn)
+        .select((book, ratings::dsl::rating))
+        .first::<BookRating>(conn)
         .map_err(Into::into)
 }

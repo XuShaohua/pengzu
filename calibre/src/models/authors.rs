6b4f83e022bb2547c9a9acc5e2170e76f700bbd7
Xu Shaohua <shaohua@biofan.org>
2022-06-02 14:40:22 +0800

    caligre: Add publishers api

--- calibre/src/models/authors.rs
+++ calibre/src/models/authors.rs
@@ -2,7 +2,9 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-use diesel::Queryable;
+use diesel::{QueryDsl, Queryable, RunQueryDsl, SqliteConnection};
+
+use crate::error::Error;
 
 #[derive(Debug, Queryable)]
 pub struct Author {
@@ -10,4 +12,12 @@ pub struct Author {
     pub name: String,
     pub sort: Option<String>,
     pub link: String,
+}
+
+pub fn get_authors(conn: &SqliteConnection, offset: i64) -> Result<Vec<Author>, Error> {
+    use crate::schema::authors::dsl::authors;
+    authors
+        .offset(offset)
+        .load::<Author>(conn)
+        .map_err(Into::into)
 }
--- calibre/src/models/publishers.rs
+++ calibre/src/models/publishers.rs
@@ -1,3 +1,21 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
+
+use crate::error::Error;
+use diesel::{QueryDsl, Queryable, RunQueryDsl, SqliteConnection};
+
+#[derive(Debug, Queryable)]
+pub struct Publisher {
+    pub id: i32,
+    pub name: String,
+    pub sort: Option<String>,
+}
+
+pub fn get_publishers(conn: &SqliteConnection, offset: i64) -> Result<Vec<Publisher>, Error> {
+    use crate::schema::publishers::dsl::publishers;
+    publishers
+        .offset(offset)
+        .load::<Publisher>(conn)
+        .map_err(Into::into)
+}

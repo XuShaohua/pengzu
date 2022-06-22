38e81205890db1dfad46f3761a74ffc3f043b0ff
Xu Shaohua <shaohua@biofan.org>
2022-06-22 16:48:41 +0800

    frontend: init

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,35 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use web_sys::{Document, HtmlElement, Window};
+use yew::{html, Component, Context, Html, NodeRef};
+
+enum Msg {}
+
+struct Model {}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
 }

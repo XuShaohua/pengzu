e0a4764122e2628954d4663ba87217840564a71b
Xu Shaohua <shaohua@biofan.org>
2022-07-08 13:22:31 +0800

    backend: Get book count in author list

--- backend/src/models/authors.rs
+++ backend/src/models/authors.rs
@@ -3,7 +3,10 @@
 // that can be found in the LICENSE file.
 
 use chrono::NaiveDateTime;
-use diesel::{ExpressionMethods, Insertable, PgConnection, QueryDsl, Queryable, RunQueryDsl};
+use diesel::{
+    ExpressionMethods, GroupByDsl, Insertable, JoinOnDsl, PgConnection, QueryDsl, Queryable,
+    RunQueryDsl,
+};
 use serde::{Deserialize, Serialize};
 
 use super::common_page;
@@ -38,29 +41,75 @@ pub fn add_author(conn: &PgConnection, new_author: &NewAuthor) -> Result<(), Err
 pub struct GetAuthorsQuery {
     #[serde(default = "common_page::default_page_id")]
     pub page: i64,
+    #[serde(default = "default_has_book")]
+    pub has_book: bool,
+}
+
+#[must_use]
+pub const fn default_has_book() -> bool {
+    true
+}
+
+#[derive(Debug, Serialize, Queryable)]
+pub struct AuthorAndBooks {
+    pub id: i32,
+    pub name: String,
+    pub link: String,
+    pub count: i64,
 }
 
 #[derive(Debug, Serialize)]
 pub struct GetAuthorsResp {
     pub page: common_page::Page,
-    pub list: Vec<Author>,
+    pub list: Vec<AuthorAndBooks>,
 }
 
 pub fn get_authors(conn: &PgConnection, query: &GetAuthorsQuery) -> Result<GetAuthorsResp, Error> {
     log::info!("query: {:?}", query);
 
-    use crate::schema::authors::dsl::authors;
+    use crate::schema::books_authors_link;
 
     let page_id = if query.page < 1 { 0 } else { query.page - 1 };
-    let each_page = 50;
+    let each_page = 100;
     let offset = page_id * each_page;
 
-    let list = authors
-        .limit(each_page)
-        .offset(offset)
-        .load::<Author>(conn)?;
+    let list = if query.has_book {
+        authors::table
+            .inner_join(books_authors_link::table.on(books_authors_link::author.eq(authors::id)))
+            .group_by(authors::id)
+            .select((
+                authors::id,
+                authors::name,
+                authors::link,
+                diesel::dsl::sql::<diesel::sql_types::BigInt>("count(books_authors_link.id)"),
+            ))
+            .limit(each_page)
+            .offset(offset)
+            .load::<AuthorAndBooks>(conn)?
+    } else {
+        let list = authors::table
+            .limit(each_page)
+            .offset(offset)
+            .load::<Author>(conn)?;
+        list.into_iter()
+            .map(|author| AuthorAndBooks {
+                id: author.id,
+                name: author.name,
+                link: author.link,
+                count: 0,
+            })
+            .collect()
+    };
 
-    let total = authors.count().first(conn)?;
+    let total = if query.has_book {
+        books_authors_link::table
+            .select(diesel::dsl::sql::<diesel::sql_types::BigInt>(
+                "count(distinct(books_authors_link.author))",
+            ))
+            .first(conn)?
+    } else {
+        authors::table.count().first(conn)?
+    };
 
     Ok(GetAuthorsResp {
         page: common_page::Page {

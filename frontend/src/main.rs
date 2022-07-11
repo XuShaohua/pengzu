b47aabba76b9c9933960bf5969bb15327ce44ab5
Xu Shaohua <shaohua@biofan.org>
2022-07-11 08:46:15 +0800

    frontend: Add component mod

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,34 +2,9 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-use web_sys::{Document, HtmlElement, Window};
-use yew::{html, Component, Context, Html, NodeRef};
-
-enum Msg {}
-
-struct Model {}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        false
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <div>
-            </div>
-        }
-    }
-}
+use frontend::app::AppComponent;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<Model>();
+    yew::start_app::<AppComponent>();
 }

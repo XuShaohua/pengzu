574be02bd5f46f4f5fa501644f7d6ad0f14565ab
Xu Shaohua <shaohua@biofan.org>
2022-07-11 10:16:10 +0800

    frontend: Add all components

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -2,6 +2,16 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+pub mod authors;
+pub mod books;
+pub mod categories;
+pub mod discover;
+pub mod file_formats;
 pub mod footer;
 pub mod header;
 pub mod left_panel;
+pub mod not_found;
+pub mod publishers;
+pub mod ratings;
+pub mod series;
+pub mod tags;
---
+++ frontend/src/components/ratings.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+use yew::prelude::*;
+
+pub enum Msg {}
+
+pub struct RatingsComponent {}
+
+impl Component for RatingsComponent {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+            {"ratings"}
+            </div>
+        }
+    }
+}

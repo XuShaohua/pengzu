747adc50cda2068c4efa538cb46a64718a9a6b4f
Xu Shaohua <shaohua@biofan.org>
2022-07-11 08:51:03 +0800

    frontend: Add lint config

--- frontend/src/components/left_panel.rs
+++ frontend/src/components/left_panel.rs
@@ -1,14 +1,3 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
-
-#![deny(
-    warnings,
-    clippy::all,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::pedantic
-)]
-
-pub mod app;
-pub mod components;

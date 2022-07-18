d23d345456cdc476d9a0848d6a626dbb2bf418bb
Xu Shaohua <shaohua@biofan.org>
2022-07-18 08:41:23 +0800

    backend: Cleanup BookAuthor

--- backend/src/models/books_authors.rs
+++ backend/src/models/books_authors.rs
@@ -2,11 +2,8 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-use chrono::NaiveDateTime;
-use diesel::{
-    ExpressionMethods, Insertable, JoinOnDsl, PgConnection, QueryDsl, Queryable, RunQueryDsl,
-};
-use serde::{Deserialize, Serialize};
+use diesel::{ExpressionMethods, Insertable, JoinOnDsl, PgConnection, QueryDsl, RunQueryDsl};
+use serde::Deserialize;
 
 use crate::error::Error;
 use crate::models::authors::Author;
@@ -19,19 +16,26 @@ pub struct NewBookAuthor {
     pub author: i32,
 }
 
-#[derive(Debug, Serialize, Queryable)]
-pub struct BookAuthor {
-    pub id: i32,
-    pub book: i32,
-    pub author: i32,
-    pub created: NaiveDateTime,
-}
-
 pub fn add_book_author(conn: &PgConnection, new_book_author: &NewBookAuthor) -> Result<(), Error> {
     use crate::schema::books_authors_link::dsl::books_authors_link;
     diesel::insert_into(books_authors_link)
         .values(new_book_author)
         .execute(conn)?;
+    Ok(())
+}
+
+pub fn delete_book_author(
+    conn: &PgConnection,
+    new_book_author: &NewBookAuthor,
+) -> Result<(), Error> {
+    use crate::schema::books_authors_link::dsl::{author, book, books_authors_link};
+    // TODO(Shaohua): Check exists
+    diesel::delete(
+        books_authors_link
+            .filter(book.eq(new_book_author.book))
+            .filter(author.eq(new_book_author.author)),
+    )
+    .execute(conn)?;
     Ok(())
 }
 
@@ -50,19 +54,4 @@ pub fn get_authors_by_book(conn: &PgConnection, book_id: i32) -> Result<Vec<Auth
         ))
         .load::<Author>(conn)
         .map_err(Into::into)
-}
-
-pub fn delete_book_author(
-    conn: &PgConnection,
-    new_book_author: &NewBookAuthor,
-) -> Result<(), Error> {
-    use crate::schema::books_authors_link::dsl::{author, book, books_authors_link};
-    // TODO(Shaohua): Check exists
-    diesel::delete(
-        books_authors_link
-            .filter(book.eq(new_book_author.book))
-            .filter(author.eq(new_book_author.author)),
-    )
-    .execute(conn)?;
-    Ok(())
 }

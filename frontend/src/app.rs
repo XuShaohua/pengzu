857d99a53fb14710ce584008ee15c4b340f38255
Xu Shaohua <shaohua@biofan.org>
2022-07-22 10:14:48 +0800

    frontend: Move /login page out of default router

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -2,17 +2,34 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+use web_sys::Url;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 use crate::components::{
     footer::FooterComponent, header::HeaderComponent, left_panel::LeftPanelComponent,
+    login::LoginComponent,
 };
 use crate::route::{switch_route, Route};
 
 pub enum Msg {}
 
 pub struct AppComponent {}
+
+fn is_login_page() -> bool {
+    let window = gloo_utils::window();
+    let location = window.location();
+    let href = match location.href() {
+        Ok(href) => href,
+        Err(_err) => return false,
+    };
+    let url = match Url::new(&href) {
+        Ok(url) => url,
+        Err(_err) => return false,
+    };
+    let pathname = url.pathname();
+    pathname == "/login"
+}
 
 impl Component for AppComponent {
     type Message = Msg;
@@ -27,6 +44,12 @@ impl Component for AppComponent {
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
+        if is_login_page() {
+            return html! {
+                <LoginComponent />
+            };
+        }
+
         html! {
             <>
                 <BrowserRouter>

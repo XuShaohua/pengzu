327653dd6601d5b82b3073a0cf899ee73ea768ec
Xu Shaohua <shaohua@biofan.org>
2022-08-23 11:24:39 +0800

    frontend: Simplify publishers component

--- frontend/src/views/publishers.rs
+++ frontend/src/views/publishers.rs
@@ -3,88 +3,33 @@
 // that can be found in the LICENSE file.
 
 use yew::prelude::*;
+use yew_hooks::{use_async_with_options, UseAsyncOptions};
 use yew_router::prelude::Link;
 
-use crate::error::FetchError;
 use crate::route::Route;
 use crate::services::publishers::fetch_publishers;
-use crate::types::page::Page;
-use crate::types::publishers::{PublisherAndBook, PublisherList};
 
-pub enum Msg {
-    Fetch,
-    FetchSuccess(PublisherList),
-    FetchFailed(FetchError),
-}
+#[function_component(PublishersComponent)]
+pub fn publishers_page() -> Html {
+    let publisher_list = use_async_with_options(
+        async move { fetch_publishers().await },
+        UseAsyncOptions::enable_auto(),
+    );
 
-pub struct PublishersComponent {
-    publishers: Vec<PublisherAndBook>,
-    page: Option<Page>,
-}
-
-fn generate_publisher_element(publisher: &PublisherAndBook) -> Html {
-    html! {
-        <li class="publisher-item" key={ publisher.id }>
-            <span class="badge">{ publisher.count }</span>
-            <Link<Route> to={ Route::BooksOfPublisher { publisher_id: publisher.id } }>
-                { &publisher.name }
-            </Link<Route>>
-        </li>
-    }
-}
-
-impl Component for PublishersComponent {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            publishers: vec![],
-            page: None,
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::Fetch => {
-                ctx.link().send_future(async {
-                    match fetch_publishers().await {
-                        Ok(obj) => Msg::FetchSuccess(obj),
-                        Err(err) => Msg::FetchFailed(err),
-                    }
-                });
-                false
-            }
-            Msg::FetchSuccess(obj) => {
-                log::info!("obj: {:#?}", obj);
-                self.page = Some(obj.page);
-                self.publishers.extend(obj.list);
-                true
-            }
-            Msg::FetchFailed(err) => {
-                log::warn!("failed to fetch publishers: {:?}", err);
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let fetch = ctx.link().callback(|_| Msg::Fetch);
-
-        let publisher_elements = self
-            .publishers
-            .iter()
-            .map(generate_publisher_element)
-            .collect::<Html>();
-
-        html! {
-            <>
-                <button onclick={ fetch }>{"Fetch publishers"}</button>
-
-                <ul class="publisher-list">
-                    { publisher_elements }
-                </ul>
-            </>
-        }
+    if let Some(publisher_list) = &publisher_list.data {
+        return html! {
+             <ul class="publisher-list">
+                {for publisher_list.list.iter().map(|publisher| html!{
+                 <li class="publisher-item" key={ publisher.id }>
+                    <span class="badge">{ publisher.count }</span>
+                    <Link<Route> to={ Route::BooksOfPublisher { publisher_id: publisher.id } }>
+                    { &publisher.name }
+                    </Link<Route>>
+                </li>
+                })}
+             </ul>
+        };
+    } else {
+        return html! {};
     }
 }

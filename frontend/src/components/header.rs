ebe62c52605c51e9e07451b4983828b329b81654
Xu Shaohua <shaohua@biofan.org>
2022-08-30 16:52:53 +0800

    frontend: Hide login button

--- frontend/src/components/header.rs
+++ frontend/src/components/header.rs
@@ -3,13 +3,25 @@
 // that can be found in the LICENSE file.
 
 use yew::prelude::*;
+use yew_router::prelude::Link;
+
+use crate::hooks::use_user_context;
+use crate::route::Route;
 
 #[function_component(HeaderComponent)]
 pub fn header() -> Html {
+    let user_ctx = use_user_context();
+
     html! {
         <div class="navbar">
             { "Online Library" }
-            <a href="/login">{"Login"}</a>
+            <div class="user-container">
+            if user_ctx.is_login() {
+                <span>{ &user_ctx.name }</span>
+            } else {
+                <Link<Route> to={ Route::Login }>{ "Login" }</Link<Route>>
+            }
+            </div>
         </div>
     }
 }

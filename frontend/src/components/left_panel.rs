6c7df456612bde9baaa25963dd5d1be8142486f9
Xu Shaohua <shaohua@biofan.org>
2022-10-17 14:07:59 +0800

    frontend: Add glyphicon style

--- frontend/src/components/left_panel.rs
+++ frontend/src/components/left_panel.rs
@@ -12,19 +12,49 @@ pub fn left_panel() -> Html {
     html! {
         <div class="left-panel">
             <ul>
-                <li><Link<Route> to={ Route::Home }>{ "Books" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Author }>{ "Authors" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Category }>{ "Categories" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Tag }>{ "Tags" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Publisher }>{ "Publishers" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Series }>{ "Series" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Discover }>{ "Discover" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::FileFormat }>{ "File Formats" }</Link<Route>></li>
-                <li><Link<Route> to={ Route::Rating }>{ "Ratings" }</Link<Route>></li>
+                <li><Link<Route> to={ Route::Home }>
+                    <span class="glyphicon glyphicon-book" />
+                    { "Books" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Author }>
+                    <span class="glyphicon glyphicon-user" />
+                    { "Authors" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Category }>
+                    <span class="glyphicon glyphicon-inbox" />
+                    { "Categories" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Tag }>
+                    <span class="glyphicon glyphicon-tags" />
+                    { "Tags" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Publisher }>
+                    <span class="glyphicon glyphicon-text-size" />
+                    { "Publishers" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Series }>
+                    <span class="glyphicon glyphicon-th" />
+                    { "Series" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Discover }>
+                    <span class="glyphicon glyphicon-random" />
+                    { "Discover" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::FileFormat }>
+                    <span class="glyphicon glyphicon-file" />
+                    { "File Formats" }
+                </Link<Route>></li>
+                <li><Link<Route> to={ Route::Rating }>
+                    <span class="glyphicon glyphicon-star-empty" />
+                    { "Ratings" }
+                </Link<Route>></li>
             </ul>
             <br />
             <ul>
-                <li><Link<Route> to={ Route::UserTag }>{ "My Tags" }</Link<Route>></li>
+                <li><Link<Route> to={ Route::UserTag }>
+                    <span class="glyphicon glyphicon-tag" />
+                    { "My Tags" }
+                </Link<Route>></li>
             </ul>
         </div>
     }

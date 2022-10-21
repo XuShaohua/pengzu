a3166977a2f342075cbfd589a7eea4dd0e4c0dc0
Xu Shaohua <shaohua@biofan.org>
2022-10-21 09:16:53 +0800

    frontend: Add style file for advanced search

--- frontend/src/views/advanced_search.rs
+++ frontend/src/views/advanced_search.rs
@@ -2,13 +2,16 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+use stylist::Style;
 use yew::prelude::*;
 
 #[function_component(AdvancedSearchComponent)]
 pub fn advanced_search() -> Html {
+    let style_str = include_str!("advanced_search.css");
+    let style_cls = Style::new(style_str).expect("Invalid style file advanced_search.css");
+
     html! {
-        <form class="advanced-search"
-            method="GET">
+        <form class={ style_cls } method="GET">
         <div class="form-group">
             <label for="book_title">{ "Book Title" }</label>
             <input id="book_title" class="form-control" name="book_title" type="text" />

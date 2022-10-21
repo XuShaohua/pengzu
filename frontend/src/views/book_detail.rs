894b73beefca132b4a494c4bbe3603ac72a3254c
Xu Shaohua <shaohua@biofan.org>
2022-10-21 09:24:29 +0800

    frontend: Add style file for book_detail

--- frontend/src/views/book_detail.rs
+++ frontend/src/views/book_detail.rs
@@ -2,6 +2,7 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+use stylist::Style;
 use yew::prelude::*;
 use yew_hooks::prelude::*;
 use yew_router::prelude::*;
@@ -106,8 +107,11 @@ fn generate_metadata_element(metadata: &BookMetadata) -> Html {
         })
         .collect::<Html>();
 
+    let style_str = include_str!("book_detail.css");
+    let style_cls = Style::new(style_str).expect("Invalid style file book_detail.css");
+
     html! {
-        <>
+        <div class={ style_cls }>
             <h2>{ &metadata.book.title }</h2>
             <div class="book-cover">
                 <img class="detail-cover" src={ get_cover_image_url(&book.small_cover) } alt={ book.title.clone() } />
@@ -118,7 +122,7 @@ fn generate_metadata_element(metadata: &BookMetadata) -> Html {
             <div class="book-tags">{ tags_element }</div>
             <div class="book-series">{ series_element }</div>
             <ul class="book-formats">{ formats_element }</ul>
-        </>
+        </div>
     }
 }
 

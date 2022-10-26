5213488861b4a2691523e898142df64b4b4a2183
Xu Shaohua <shaohua@biofan.org>
2022-10-26 10:29:12 +0800

    frontend: Add buttons to pagination

--- frontend/src/components/book_pagination.rs
+++ frontend/src/components/book_pagination.rs
@@ -15,21 +15,82 @@ pub struct Props {
 
 #[function_component(BookPaginationComponent)]
 pub fn book_pagination(props: &Props) -> Html {
-    log::info!(
-        "current page: {}, total pages: {}",
-        props.current_page,
-        props.total_pages
-    );
-
     let style_str = include_str!("book_pagination.css");
     let style_cls = Style::new(style_str).expect("Invalid style file book_pagination.css");
 
+    let has_previous = props.current_page > 1;
+    let has_next = props.current_page + 1 < props.total_pages;
+
+    let mut pages = Vec::new();
+    let min_pages = 12;
+    if props.total_pages < min_pages {
+        for i in 1..props.total_pages {
+            pages.push(Some(i));
+        }
+    } else {
+        let left_page = 5;
+        if props.current_page < left_page && props.total_pages > left_page {
+            for i in 1..left_page + 1 {
+                pages.push(Some(i));
+            }
+        }
+        pages.push(None);
+
+        let right_page = props.total_pages - left_page;
+        for i in right_page..props.total_pages {
+            pages.push(Some(i));
+        }
+    }
+
+    let buttons = pages
+        .iter()
+        .map(|page_id| {
+            page_id.map_or_else(
+                || {
+                    html! {
+                        <li class="page">
+                            <button class="btn" disabled={true}>{ ".." }</button>
+                        </li>
+                    }
+                },
+                |page_id| {
+                    let props_onclick = props.onclick.clone();
+                    let onclick = Callback::from(move |event: MouseEvent| {
+                        event.prevent_default();
+                        props_onclick.emit(page_id);
+                    });
+
+                    let button_cls = if page_id == props.current_page {
+                        "page active"
+                    } else {
+                        "page"
+                    };
+
+                    html! {
+                        <li class={ button_cls }>
+                            <button onclick={onclick} class="btn">{ page_id }</button>
+                        </li>
+                    }
+                },
+            )
+        })
+        .collect::<Html>();
+
     html! {
-        <div class={ style_cls }>
-            <button>{ "Previous" }</button>
-            <button>{ props.current_page }</button>
-            <button>{ props.total_pages }</button>
-            <button>{ "Next" }</button>
-        </div>
+        <ul class={ style_cls }>
+            if has_previous {
+                <li class="page previous">
+                    <button class="btn">{ "« Previous" }</button>
+                </li>
+            }
+
+            { buttons }
+
+            if has_next {
+                <li class="page next">
+                    <button class="btn">{ "Next »" }</button>
+                </li>
+            }
+        </ul>
     }
 }

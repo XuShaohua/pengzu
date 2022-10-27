49c2367b53f14d482b323540285eac4ca763d1c3
Xu Shaohua <shaohua@biofan.org>
2022-10-27 08:25:53 +0800

    frontend: Add pagination to booksOfSimpleSearch

--- frontend/src/views/books_of_simple_search.rs
+++ frontend/src/views/books_of_simple_search.rs
@@ -4,23 +4,42 @@
 
 use yew::prelude::*;
 use yew_hooks::{use_async_with_options, UseAsyncOptions};
-use yew_router::history::Location;
-use yew_router::hooks::use_location;
+use yew_router::history::{History, Location};
+use yew_router::hooks::{use_history, use_location};
 
 use crate::components::book_list::BookListComponent;
+use crate::components::book_pagination::BookPaginationComponent;
+use crate::router::Route;
 use crate::services::books::fetch_books_by_simple_search;
+use crate::types::page::PageId;
 use crate::types::simple_search::SimpleSearchQuery;
 
 #[function_component(BooksOfSimpleSearchComponent)]
 pub fn books_of_simple_search() -> Html {
+    // TODO(Shaohua): Set document title.
+
+    let history = use_history().unwrap();
     let location = use_location().unwrap();
     let query = location.query::<SimpleSearchQuery>().unwrap_or_default();
     let keyword = query.query.clone();
+    let book_list = {
+        let query_clone = query.clone();
+        use_async_with_options(
+            async move { fetch_books_by_simple_search(&query_clone).await },
+            UseAsyncOptions::enable_auto(),
+        )
+    };
 
-    let book_list = use_async_with_options(
-        async move { fetch_books_by_simple_search(&query).await },
-        UseAsyncOptions::enable_auto(),
-    );
+    let pagination_onclick = {
+        Callback::from(move |page_id: PageId| {
+            let new_query = SimpleSearchQuery {
+                page: page_id,
+                ..query.clone()
+            };
+            let ret = history.push_with_query(Route::BooksOfSimpleSearch, &new_query);
+            debug_assert!(ret.is_ok());
+        })
+    };
 
     book_list.data.as_ref().map_or_else(
         || {
@@ -33,6 +52,9 @@ pub fn books_of_simple_search() -> Html {
                 <>
                 <h2>{ book_list.page.total }{ " Results for \""}{ &keyword }{"\""}</h2>
                 <BookListComponent books={ book_list.list.clone() } />
+                <BookPaginationComponent current_page={ book_list.page.page_num }
+                    total_pages={ book_list.page.total_pages() }
+                    onclick={ pagination_onclick } />
                 </>
             }
         },

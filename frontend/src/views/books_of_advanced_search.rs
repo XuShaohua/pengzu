d5fb2ab42c166678969071130d7403c192832d26
Xu Shaohua <shaohua@biofan.org>
2022-11-07 14:01:42 +0800

    frontend: Make advanced search reentrant

--- frontend/src/views/books_of_advanced_search.rs
+++ frontend/src/views/books_of_advanced_search.rs
@@ -3,9 +3,9 @@
 // that can be found in the LICENSE file.
 
 use yew::prelude::*;
-use yew_hooks::{use_async_with_options, UseAsyncOptions};
+use yew_hooks::use_async;
 use yew_router::history::{History, Location};
-use yew_router::hooks::{use_history, use_location};
+use yew_router::hooks::use_history;
 
 use crate::components::book_filter::BookFilterComponent;
 use crate::components::book_list::BookListComponent;
@@ -20,18 +20,26 @@ use crate::views::util;
 #[function_component(BooksOfAdvancedSearchComponent)]
 pub fn books_of_advanced_search() -> Html {
     let history = use_history().unwrap();
-    let location = use_location().unwrap();
+    let location = history.location();
     let query = location.query::<AdvancedSearchQuery>().unwrap_or_default();
     let query_desc = query.desc();
     util::set_document_title(&format!("Advanced Search: {}", query_desc));
 
     let book_list = {
         let query_clone = query.clone();
-        use_async_with_options(
-            async move { fetch_books_by_advanced_search(&query_clone).await },
-            UseAsyncOptions::enable_auto(),
-        )
+        use_async(async move { fetch_books_by_advanced_search(&query_clone).await })
     };
+
+    {
+        let book_list_clone = book_list.clone();
+        use_effect_with_deps(
+            move |_query_clone| {
+                book_list_clone.run();
+                || ()
+            },
+            query.clone(),
+        );
+    }
 
     let book_filter_onchange = {
         Callback::from(|order: GetBooksOrder| {

d1de7dce92af8319819553b21f83e284c589abb1
Xu Shaohua <shaohua@biofan.org>
2022-11-25 11:24:55 +0800

    backend: Sort simple search result

--- backend/src/models/simple_search.rs
+++ backend/src/models/simple_search.rs
@@ -5,10 +5,12 @@
 use diesel::{PgConnection, PgTextExpressionMethods, QueryDsl, RunQueryDsl};
 use shared::books::BookAndAuthorsList;
 use shared::books_query::GetBooksQuery;
+use shared::page::BOOKS_EACH_PAGE;
 use shared::simple_search::SimpleSearchQuery;
 
 use crate::error::Error;
-use crate::models::books::get_books_by_ids;
+use crate::models::books::{book_list_to_book_authors, Book};
+use crate::models::books_query::sort_books_by_column;
 
 pub fn get_books_by_simple_search(
     conn: &mut PgConnection,
@@ -16,17 +18,24 @@ pub fn get_books_by_simple_search(
 ) -> Result<BookAndAuthorsList, Error> {
     use crate::schema::books;
 
-    let query_pattern = format!("%{}%", query.query);
     let books_query = GetBooksQuery {
         page: query.page,
         order: query.order,
     };
+    let offset = books_query.backend_page_id() * BOOKS_EACH_PAGE;
+    let query_pattern = format!("%{}%", query.query);
 
-    // TODO(Shaohua): Remove get_books_by_ids()
-    let book_ids = books::table
+    let total = books::table
         .filter(books::title.ilike(&query_pattern))
-        .select(books::id)
-        .load::<i32>(conn)?;
+        .count()
+        .first::<i64>(conn)?;
 
-    get_books_by_ids(conn, &books_query, &book_ids)
+    // Get book list based on a subquery.
+    let book_list = sort_books_by_column(query.order)
+        .filter(books::title.ilike(&query_pattern))
+        .limit(BOOKS_EACH_PAGE)
+        .offset(offset)
+        .load::<Book>(conn)?;
+
+    book_list_to_book_authors(conn, book_list, &books_query, total)
 }

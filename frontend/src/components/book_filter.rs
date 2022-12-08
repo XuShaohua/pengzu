a451631f5a1c5bf2ec7a949d03e4242b56697583
Xu Shaohua <shaohua@biofan.org>
2022-12-08 09:35:14 +0800

    frontend: Add a button group in book filter

--- frontend/src/components/book_filter.rs
+++ frontend/src/components/book_filter.rs
@@ -3,7 +3,6 @@
 // that can be found in the LICENSE file.
 
 use shared::books_query::GetBooksOrder;
-use stylist::Style;
 use yew::prelude::*;
 
 #[derive(Debug, PartialEq, Properties)]
@@ -14,9 +13,6 @@ pub struct Props {
 
 #[function_component(BookFilterComponent)]
 pub fn book_filter(props: &Props) -> Html {
-    let style_str = include_str!("book_filter.css");
-    let style_cls = Style::new(style_str).expect("Invalid book_filter.css file");
-
     let button_onclick = |order: GetBooksOrder| {
         let onchange = props.onchange.clone();
         Callback::from(move |event: MouseEvent| {
@@ -37,50 +33,50 @@ pub fn book_filter(props: &Props) -> Html {
     };
 
     html! {
-        <div class={ style_cls }>
+        <div class="btn-group mt-2 mb-2">
 
-        <button class={ get_button_cls(GetBooksOrder::IdAsc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::IdAsc) }
             title={ "Sort according to book date, newest first" }
             onclick={ button_onclick(GetBooksOrder::IdAsc) }>
             <i class="bi bi-sort-numeric-down"></i>
         </button>
-        <button class={ get_button_cls(GetBooksOrder::IdDesc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::IdDesc) }
             title={ "Sort according to book date, oldest first" }
             onclick={ button_onclick(GetBooksOrder::IdDesc) }>
             <i class="bi bi-sort-numeric-down-alt"></i>
         </button>
 
-        <button class={ get_button_cls(GetBooksOrder::TitleAsc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::TitleAsc) }
             title={ "Sort titles in alphabetical order" }
             onclick={ button_onclick(GetBooksOrder::TitleAsc) }>
             <i class="bi bi-sort-alpha-down"></i>
         </button>
-        <button class={ get_button_cls(GetBooksOrder::TitleDesc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::TitleDesc) }
             title={ "Sort titles in reverse alphabetical order" }
             onclick={ button_onclick(GetBooksOrder::TitleDesc) }>
             <i class="bi bi-sort-alpha-down-alt"></i>
         </button>
 
-        <button class={ get_button_cls(GetBooksOrder::AuthorAsc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::AuthorAsc) }
             title={ "Sort authors in alphabetical order" }
             onclick={ button_onclick(GetBooksOrder::AuthorAsc) }>
             <i class="bi bi-person"></i>
             <i class="bi bi-sort-alpha-down-alt"></i>
         </button>
-        <button class={ get_button_cls(GetBooksOrder::AuthorDesc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::AuthorDesc) }
             title={ "Sort authors in reverse alphabetical order" }
             onclick={ button_onclick(GetBooksOrder::AuthorDesc) }>
             <i class="bi bi-person"></i>
             <i class="bi bi-sort-alpha-down-alt"></i>
         </button>
 
-        <button class={ get_button_cls(GetBooksOrder::PubdateAsc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::PubdateAsc) }
             title={ "Sort according to publishing date, newest first" }
             onclick={ button_onclick(GetBooksOrder::PubdateAsc) }>
             <i class="bi bi-calendar-date"></i>
             <i class="bi bi-sort-numeric-down"></i>
         </button>
-        <button class={ get_button_cls(GetBooksOrder::PubdateDesc) }
+        <button type="button" class={ get_button_cls(GetBooksOrder::PubdateDesc) }
             title={ "Sort according to publishing date, oldest first" }
             onclick={ button_onclick(GetBooksOrder::PubdateDesc) }>
             <i class="bi bi-calendar-date"></i>

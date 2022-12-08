f6c1991da7c9f651694958b979bad0acaea13a35
Xu Shaohua <shaohua@biofan.org>
2022-12-08 15:08:48 +0800

    frontend: Remove style file of footer

--- frontend/src/components/footer.rs
+++ frontend/src/components/footer.rs
@@ -2,16 +2,12 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
-use stylist::Style;
 use yew::prelude::*;
 
 #[function_component(FooterComponent)]
 pub fn footer() -> Html {
-    let style_str = include_str!("footer.css");
-    let style_cls = Style::new(style_str).expect("Invalid style file footer.css");
-
     html! {
-        <footer class={ style_cls }>
+        <footer class="mt-3 fw-light">
             {"© 2022 Shaohua"}
         </footer>
     }

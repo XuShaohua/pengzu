10ecac5c9a523ade47ebc1101576d2c362cdba6a
Xu Shaohua <shaohua@biofan.org>
2022-12-09 14:14:42 +0800

    frontend: Update pagination in user publisher

--- frontend/src/views/publishers.rs
+++ frontend/src/views/publishers.rs
@@ -7,8 +7,7 @@ use shared::page::PageId;
 use shared::publishers::PublisherAndBook;
 use yew::prelude::*;
 use yew_hooks::use_async;
-use yew_router::hooks::{use_location, use_navigator};
-use yew_router::prelude::Link;
+use yew_router::prelude::{use_location, Link};
 
 use crate::components::general_filter::GeneralFilterComponent;
 use crate::components::pagination::PaginationComponent;
@@ -35,12 +34,14 @@ fn generate_publisher_list(publisher_list: &[PublisherAndBook]) -> Html {
 pub fn publishers_page() -> Html {
     util::set_document_title("Publishers");
 
-    let navigator = use_navigator().unwrap();
     let location = use_location().unwrap();
     let query = location.query::<GeneralQuery>().unwrap_or_default();
     let publisher_list = {
         let query_clone = query.clone();
-        use_async(async move { fetch_publishers(&query_clone).await })
+        use_async(async move {
+            util::scroll_to_top();
+            fetch_publishers(&query_clone).await
+        })
     };
     {
         let publisher_list_clone = publisher_list.clone();
@@ -59,18 +60,22 @@ pub fn publishers_page() -> Html {
         })
     };
 
-    let on_pagination_click = {
+    let pagination_link = {
         let query_clone = query.clone();
-        Callback::from(move |page_id: PageId| {
-            util::scroll_to_top();
-
-            let new_query = GeneralQuery {
-                page: page_id,
-                ..query_clone
-            };
-            let ret = navigator.push_with_query(&Route::Publisher, &new_query);
-            debug_assert!(ret.is_ok());
-        })
+        Callback::from(
+            move |(page_id, classes, title): (PageId, &'static str, String)| -> Html {
+                let new_query = GeneralQuery {
+                    page: page_id,
+                    ..query_clone
+                };
+                html! {
+                    <Link<Route, GeneralQuery> to={ Route::Publisher }
+                        query={ Some(new_query) } classes={ classes }>
+                        { title }
+                    </Link<Route, GeneralQuery>>
+                }
+            },
+        )
     };
 
     publisher_list.data.as_ref().map_or_else(
@@ -91,7 +96,7 @@ pub fn publishers_page() -> Html {
 
                 <PaginationComponent  current_page={ publisher_list.page.page_num }
                     total_pages={ publisher_list.page.total_pages() }
-                    onclick={ on_pagination_click } />
+                    link={ pagination_link } />
                 </>
             }
         },

94ea8e09315faadcdd88b4aafb42c07ab312ec54
Xu Shaohua <shaohua@biofan.org>
2022-12-12 16:35:04 +0800

    frontend: Save user info to storage

--- frontend/src/components/user_context_provider.rs
+++ frontend/src/components/user_context_provider.rs
@@ -7,7 +7,7 @@ use yew::prelude::*;
 use yew_hooks::{use_async, use_mount};
 
 use crate::error::ErrorKind;
-use crate::services::auth::{get_token, set_token};
+use crate::services::auth::{get_token, load_user_info, save_user_info, set_token};
 use crate::services::users::get_user_info;
 
 #[derive(Clone, PartialEq, Properties)]
@@ -17,14 +17,15 @@ pub struct Props {
 
 #[function_component(UserContextProvider)]
 pub fn user_context_provider(props: &Props) -> Html {
-    let user_ctx = use_state(UserInfo::default);
+    let current_user_info = load_user_info();
+    let user_info_valid = current_user_info.is_some();
+    let user_ctx = use_state(move || current_user_info.unwrap_or_default());
     let current_user = use_async(async move { get_user_info().await });
-
     {
-        let current_user = current_user.clone();
+        let current_user_clone = current_user.clone();
         use_mount(move || {
-            if get_token().is_some() {
-                current_user.run();
+            if !user_info_valid && get_token().is_some() {
+                current_user_clone.run();
             }
         });
     }
@@ -34,6 +35,7 @@ pub fn user_context_provider(props: &Props) -> Html {
         use_effect_with_deps(
             move |current_user| {
                 if let Some(user_info) = &current_user.data {
+                    save_user_info(user_info);
                     user_ctx.set(user_info.clone());
                 }
                 if let Some(error) = &current_user.error {
--- frontend/src/services/auth.rs
+++ frontend/src/services/auth.rs
@@ -3,8 +3,10 @@
 // that can be found in the LICENSE file.
 
 use gloo_storage::Storage;
+use shared::users::UserInfo;
 
-const STORAGE_KEY_TOKEN: &str = "token";
+const STORAGE_KEY_TOKEN: &str = "auth.token";
+const STORAGE_KEY_USER_INFO: &str = "auth.user-info";
 
 #[must_use]
 pub fn get_token() -> Option<String> {
@@ -31,6 +33,29 @@ pub fn set_token(token: Option<&str>) {
             debug_assert!(!token.is_empty());
             if let Err(err) = storage.set(STORAGE_KEY_TOKEN, token) {
                 log::error!("Failed to store token to local storage, err: {:?}", err);
+            }
+        },
+    );
+}
+
+#[must_use]
+pub fn load_user_info() -> Option<UserInfo> {
+    let storage = gloo_storage::LocalStorage::raw();
+    if let Ok(Some(value)) = storage.get(STORAGE_KEY_USER_INFO) {
+        serde_json::from_str(&value).ok()
+    } else {
+        log::info!("Failed to get user info {}", STORAGE_KEY_USER_INFO);
+        None
+    }
+}
+
+pub fn save_user_info(user_info: &UserInfo) {
+    let storage = gloo_storage::LocalStorage::raw();
+    serde_json::to_string(user_info).map_or_else(
+        |err| log::warn!("Failed to series user info to string, err: {:?}", err),
+        |value| {
+            if let Err(err) = storage.set(STORAGE_KEY_USER_INFO, &value) {
+                log::warn!("Failed to save user info to storage, err: {:?}", err);
             }
         },
     );

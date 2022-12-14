a7c64d23a377ef13728c4cbf460a65c8ed248e90
Xu Shaohua <shaohua@biofan.org>
2022-12-14 15:56:38 +0800

    frontend: Add edit tag item

---
+++ frontend/src/views/tags/edit_tag_item.rs
@@ -0,0 +1,98 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by GNU General Public License
+// that can be found in the LICENSE file.
+
+use shared::recursive_query::RecursiveQuery;
+use shared::tags::TagAndBook;
+use yew::prelude::*;
+use yew_hooks::use_async;
+use yew_router::prelude::Link;
+
+use crate::router::Route;
+use crate::services::tags::{delete_tag, fetch_tags};
+
+#[derive(Debug, Clone, PartialEq, Eq, Properties)]
+pub struct Props {
+    pub tag: TagAndBook,
+}
+
+pub fn generate_edit_tag_list(tag_list: &[TagAndBook]) -> Html {
+    html! {
+        <ol class="">
+        {for tag_list.iter().map(|tag| html!{
+            <li class="mb-2" key={ tag.id }>
+                <EditTagItemComponent tag={ tag.clone() } />
+            </li>
+        })}
+        </ol>
+    }
+}
+
+#[function_component(EditTagItemComponent)]
+pub fn edit_tag_item(props: &Props) -> Html {
+    let tag = &props.tag;
+    let parent_id = tag.id;
+
+    let is_deleted = use_state(|| false);
+    let fetch_child_tags_task = use_async(async move {
+        // Always fetch all tags.
+        let query = RecursiveQuery {
+            parent: parent_id,
+            fetch_all: true,
+            ..RecursiveQuery::default()
+        };
+        fetch_tags(&query).await
+    });
+    let child_items = fetch_child_tags_task.data.as_ref().map_or_else(
+        || html! {},
+        |tag_list| generate_edit_tag_list(&tag_list.list),
+    );
+    let on_tag_click = {
+        Callback::from(move |event: MouseEvent| {
+            event.prevent_default();
+            fetch_child_tags_task.run();
+        })
+    };
+
+    let delete_tag_task = {
+        let tag_id = tag.id;
+        let is_deleted_clone = is_deleted.clone();
+        use_async(async move {
+            is_deleted_clone.set(true);
+            delete_tag(tag_id).await
+        })
+    };
+    let on_delete_button_click = {
+        Callback::from(move |event: MouseEvent| {
+            event.prevent_default();
+            delete_tag_task.run();
+        })
+    };
+    let on_edit_button_click = {
+        let tag_id = tag.id;
+        Callback::from(move |event: MouseEvent| {
+            event.prevent_default();
+            log::info!("edit tag: {}", tag_id);
+        })
+    };
+
+    html! {
+        <>
+            <button class="btn btn-secondary me-2" onclick={ on_edit_button_click }>{ "Edit" }</button>
+            <button class="btn btn-danger me-2" onclick={ on_delete_button_click }>{ "Delete" }</button>
+            <span class="badge rounded-pill d-inline me-2 text-bg-secondary">{ tag.count }</span>
+            <Link<Route> to={ Route::BooksOfTag { tag_id: tag.id }}
+                classes={ if *is_deleted { "disabled" } else {""} }>
+                { &tag.name }
+            </Link<Route>>
+            {
+                if tag.children > 0 {
+                    html! { <a href="#" onclick={on_tag_click}><i class="bi bi-caret-right"></i></a> }
+                } else {
+                    html! {}
+                }
+            }
+            { child_items }
+        </>
+    }
+}
--- frontend/src/views/tags/mod.rs
+++ frontend/src/views/tags/mod.rs
@@ -2,6 +2,9 @@
 // Use of this source is governed by GNU General Public License
 // that can be found in the LICENSE file.
 
+mod edit_tag_item;
+mod tag_item;
+
 use shared::general_query::GeneralOrder;
 use shared::page::PageId;
 use shared::recursive_query::RecursiveQuery;
@@ -11,17 +14,18 @@ use yew_router::prelude::{use_location, Link};
 
 use crate::components::general_filter::GeneralFilterComponent;
 use crate::components::pagination::PaginationComponent;
+use crate::hooks::use_user_context;
 use crate::router::Route;
 use crate::services::tags::fetch_tags;
 use crate::views::util;
-
-mod tag_item;
+use edit_tag_item::generate_edit_tag_list;
 use tag_item::generate_tag_list;
 
 #[function_component(TagsComponent)]
 pub fn tags_page() -> Html {
     util::set_document_title("Tags");
 
+    let user_ctx = use_user_context();
     let location = use_location().unwrap();
     let query = location.query::<RecursiveQuery>().unwrap_or_default();
     let tag_list = {
@@ -71,7 +75,11 @@ pub fn tags_page() -> Html {
                 <h2>{ "Tags" }</h2>
                 <GeneralFilterComponent onchange={ filter_onchange } current_order={ query.order } />
 
-                { generate_tag_list(&tag_list.list) }
+                {if user_ctx.is_admin() {
+                    generate_edit_tag_list(&tag_list.list)
+                } else {
+                    generate_tag_list(&tag_list.list)
+                }}
 
                 <PaginationComponent  current_page={ tag_list.page.page_num }
                     total_pages={ tag_list.page.total_pages() }

7fd56c89ab9e25192500f29259be928192fb81f4
Xu Shaohua <shaohua@biofan.org>
2022-12-14 17:03:04 +0800

    frontend: Support deleting series

--- frontend/src/services/series.rs
+++ frontend/src/services/series.rs
@@ -8,7 +8,7 @@ use shared::general_query::GeneralQuery;
 use shared::series::{Series, SeriesAndBookList};
 
 use crate::error::FetchError;
-use crate::services::fetch::request_get;
+use crate::services::fetch::{request_delete, request_get};
 
 /// Get series list.
 ///
@@ -40,4 +40,15 @@ pub async fn fetch_books_by_series(
     let query_str = serde_urlencoded::to_string(query)?;
     let url = format!("/api/series/books/{}?{}", series_id, query_str);
     request_get(&url).await
+}
+
+/// Delete specific series.
+///
+/// Note that this api can be trigger by admin only.
+///
+/// # Errors
+/// Returns error if server fails.
+pub async fn delete_series(series_id: i32) -> Result<(), FetchError> {
+    let url = format!("/api/series/{}", series_id);
+    request_delete(&url).await
 }

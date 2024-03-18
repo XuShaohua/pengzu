97ea08bc7b55506f25e8c4cbf8617ca3ed901136
Xu Shaohua <shaohua@biofan.org>
2024-03-18 09:43:35 +0800

    Update deps

--- backend/src/views/auth.rs
+++ backend/src/views/auth.rs
@@ -40,7 +40,7 @@ impl Claims {
             id: permission.id,
             name: permission.name.clone(),
             role: permission.role,
-            exp: (Utc::now() + Duration::hours(JWT_EXPIRATION_HOURS)).timestamp(),
+            exp: (Utc::now() + Duration::try_hours(JWT_EXPIRATION_HOURS).unwrap()).timestamp(),
         }
     }
 
